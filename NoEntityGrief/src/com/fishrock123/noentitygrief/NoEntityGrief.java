2344f3b7993516d5590813b2b1c5e9bc7f23c473
Fishrock123 <fishrock123@rocketmail.com>
2012-04-16 09:24:54 -0400

    Version 2.0.3 (1C7) * Even smaller filesize. (2.60KB)

--- NoEntityGrief/src/com/fishrock123/noentitygrief/NoEntityGrief.java
+++ NoEntityGrief/src/com/fishrock123/noentitygrief/NoEntityGrief.java
@@ -6,7 +6,7 @@ import org.bukkit.event.block.*;
 import org.bukkit.event.entity.*;
 import org.bukkit.plugin.java.JavaPlugin;
 public class NoEntityGrief extends JavaPlugin implements Listener {
-	private Map<Class<? extends Entity>, List<String>> EWs = new HashMap<Class<? extends Entity>, List<String>>();
+	private HashMap<Class<? extends Entity>, List<String>> EWs = new HashMap<Class<? extends Entity>, List<String>>();
 	public void onEnable() {
 		if (getConfig().options().header() == null) {
 			getConfig().options().copyHeader();
@@ -20,7 +20,7 @@ public class NoEntityGrief extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	@EventHandler public void onBlockChange(EntityChangeBlockEvent e) {
-		e.setCancelled(e.getEntity() instanceof Enderman && (EWs == null || !EWs.containsKey(Enderman.class) || !EWs.get(Enderman.class).contains(e.getBlock().getLocation().getWorld().getName())) ? true : e.isCancelled());
+		if (e.getEntity() instanceof Enderman && (EWs == null || !EWs.containsKey(Enderman.class) || !EWs.get(Enderman.class).contains(e.getBlock().getLocation().getWorld().getName()))) e.setCancelled(true);
 		if (e.getEntity() instanceof Sheep && (EWs == null || !EWs.containsKey(Sheep.class) || !EWs.get(Sheep.class).contains(e.getBlock().getLocation().getWorld().getName()))) {
 			e.setCancelled(true);
 			((Sheep)e.getEntity()).setSheared(false);
@@ -30,6 +30,6 @@ public class NoEntityGrief extends JavaPlugin implements Listener {
 		if (EWs == null || !EWs.containsKey(Creeper.class) || !EWs.get(Creeper.class).contains(e.getLocation().getWorld().getName())) e.blockList().clear();
 	}
 	@EventHandler public void onEntityForm(EntityBlockFormEvent e) {
-		e.setCancelled(EWs == null || !EWs.containsKey(Snowman.class) || !EWs.get(Snowman.class).contains(e.getBlock().getLocation().getWorld().getName()) ? true : e.isCancelled());
+		if (EWs == null || !EWs.containsKey(Snowman.class) || !EWs.get(Snowman.class).contains(e.getBlock().getLocation().getWorld().getName())) e.setCancelled(true);
 	}
 }
