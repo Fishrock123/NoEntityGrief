5f201c4e04661f68428d40b10be4fcdf05fad292
Fishrock123 <fishrock123@rocketmail.com>
2012-11-08 15:06:07 -0500

    Version 3.0 (1D5)
    * Hanging entities (paintings & item frames) are now protected from
    explosions!
    * Significant core changes that reduce the file-size and increase
    performance!
    * Made the default config nicer and more understandable.

--- src/com/fishrock123/noentitygrief/NoEntityGrief.java
+++ src/com/fishrock123/noentitygrief/NoEntityGrief.java
@@ -4,37 +4,43 @@ import org.bukkit.entity.*;
 import org.bukkit.event.*;
 import org.bukkit.event.block.*;
 import org.bukkit.event.entity.*;
+import org.bukkit.event.hanging.*;
 import org.bukkit.plugin.java.JavaPlugin;
 public class NoEntityGrief extends JavaPlugin implements Listener { // Licensed under GPLv3
-	private HashMap<Class<? extends Entity>, List<String>> EWs = new HashMap<Class<? extends Entity>, List<String>>();
+	@SuppressWarnings("unchecked") private List<String>[] EWs = new ArrayList[6];
 	@Override public void onEnable() {
 		if (getConfig().options().header() == null) {
 			getConfig().options().copyHeader();
 			getConfig().options().copyDefaults(true);
 			saveConfig();
 		}
-		EWs.put(Enderman.class, getConfig().getStringList("Endermen"));
-		EWs.put(Sheep.class, getConfig().getStringList("Sheep"));
-		EWs.put(Creeper.class, getConfig().getStringList("MobExplosion"));
-		EWs.put(TNTPrimed.class, getConfig().getStringList("TNT"));
-		EWs.put(Snowman.class, getConfig().getStringList("Snowman"));
-		EWs.put(Silverfish.class, getConfig().getStringList("Silverfish"));
+		EWs[0] = getConfig().getStringList("Endermen");
+		EWs[1] = getConfig().getStringList("Sheep");
+		EWs[2] = getConfig().getStringList("MobExplosion");
+		EWs[3] = getConfig().getStringList("TNT");
+		EWs[4] = getConfig().getStringList("Snowman");
+		EWs[5] = getConfig().getStringList("Silverfish");
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 	@EventHandler public void onBlockChange(EntityChangeBlockEvent e) {
-		if (e.getEntity() instanceof Enderman && (EWs == null || !EWs.containsKey(Enderman.class) || !EWs.get(Enderman.class).contains(e.getBlock().getLocation().getWorld().getName()))) e.setCancelled(true);
-		else if (e.getEntity() instanceof Silverfish && (EWs == null || !EWs.containsKey(Silverfish.class) || !EWs.get(Silverfish.class).contains(e.getBlock().getLocation().getWorld().getName()))) e.setCancelled(true);
-		else if (e.getEntity() instanceof Sheep && (EWs == null || !EWs.containsKey(Sheep.class) || !EWs.get(Sheep.class).contains(e.getBlock().getLocation().getWorld().getName()))) {
+		if (e.getEntity() instanceof Enderman && (EWs[0] == null || !EWs[0].contains(e.getBlock().getLocation().getWorld().getName()))) e.setCancelled(true);
+		else if (e.getEntity() instanceof Silverfish && (EWs[5] == null || !EWs[5].contains(e.getBlock().getLocation().getWorld().getName()))) e.setCancelled(true);
+		else if (e.getEntity() instanceof Sheep && (EWs[1] == null || !EWs[1].contains(e.getBlock().getLocation().getWorld().getName()))) {
 			e.setCancelled(true);
 			((Sheep)e.getEntity()).setSheared(false);
 		}
 	}
 	@EventHandler public void onExplosion(EntityExplodeEvent e) {
 		if (e.getEntity() instanceof TNTPrimed) { 
-			if (EWs == null || !EWs.containsKey(TNTPrimed.class) || !EWs.get(TNTPrimed.class).contains(e.getLocation().getWorld().getName())) e.blockList().clear();
-		} else if (EWs == null || !EWs.containsKey(Creeper.class) || !EWs.get(Creeper.class).contains(e.getLocation().getWorld().getName())) e.blockList().clear();
+			if (EWs[3] == null || !EWs[3].contains(e.getLocation().getWorld().getName())) e.blockList().clear();
+		} else if (EWs[2] == null || !EWs[2].contains(e.getLocation().getWorld().getName())) e.blockList().clear();
+	}
+	@EventHandler public void onPaintingBreak(HangingBreakEvent e) {
+		if (e.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION && e.getEntity() instanceof TNTPrimed) { 
+			if (EWs[3] == null || !EWs[3].contains(e.getEntity().getWorld().getName())) e.setCancelled(true);
+		} else if (e.getCause() == HangingBreakEvent.RemoveCause.EXPLOSION && (EWs[2] == null || !EWs[2].contains(e.getEntity().getWorld().getName()))) e.setCancelled(true);
 	}
 	@EventHandler public void onEntityForm(EntityBlockFormEvent e) {
-		if (EWs == null || !EWs.containsKey(Snowman.class) || !EWs.get(Snowman.class).contains(e.getBlock().getLocation().getWorld().getName())) e.setCancelled(true);
+		if (EWs[4] == null || !EWs[4].contains(e.getBlock().getLocation().getWorld().getName())) e.setCancelled(true);
 	}
 }
